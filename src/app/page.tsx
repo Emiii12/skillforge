89467ec77b6645894fc328a0e0c73ecf052bb28e
Emilio Pereira <91755231+Emiii12@users.noreply.github.com>
2025-04-25 11:30:55 -0300

    Project ready to start

---
+++ src/app/page.tsx
@@ -0,0 +1,9 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <div className="w-screen h-screen flex justify-center items-center">
+        <h1 className="text-2xl text-black">Hola mundo!</h1>
+    </div>
+  );
+}
